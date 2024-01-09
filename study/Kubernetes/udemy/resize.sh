9fa1a6f071a903e195dd5fb476459da7b24f2b22
ormazed <68933277+ormazed@users.noreply.github.com>
2024-01-09 12:27:12 +0900

    resize.sh 업데이트

--- study/Kubernetes/udemy/resize.sh
+++ study/Kubernetes/udemy/resize.sh
@@ -1,3 +1,14 @@
+# 접속방법 정리
+
+## gcloud 접속
+gcloud auth login
+gclooud config set project theta-style-403004
+./increase.sh
+
+
+
+
+```
 #!/bin/bash
 
 # reszie : 3 to 0
@@ -9,3 +20,4 @@ echo "done"
 # echo "reszie : 0 to 3"
 # gcloud container clusters resize --zone us-central1-a in28minutes-cluster --num-nodes=3
 # echo "done"
+```
