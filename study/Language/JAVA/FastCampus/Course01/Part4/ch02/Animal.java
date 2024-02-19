625d92db09667d3f742537bce26c3ba4b7eede9f
ormazed <68933277+ormazed@users.noreply.github.com>
2024-02-20 00:14:17 +0900

    Create Animal.java

---
+++ study/Language/JAVA/FastCampus/Course01/Part4/ch02/Animal.java
@@ -0,0 +1,11 @@
+package fc.java.model;
+
+public class Animal {
+    public void eat(){
+        System.out.println("동물은 먹는다");
+    }
+
+    public void bite(){
+        System.out.println("문다");
+    }
+}
