fcb41c3988523ced2054871485776315469dd3bd
ormazed <68933277+ormazed@users.noreply.github.com>
2024-02-19 23:41:36 +0900

    Create EmployeeInitTest.java

---
+++ study/Language/JAVA/FastCampus/Course01/Part4/ch01/EmployeeInitTest.java
@@ -0,0 +1,8 @@
+package fc.java.part4;
+
+public class EmployeeInitTest {
+    public static void main(String[] args) {
+        RempVO vo = new RempVO("홍길동", 33, "010-9874-3214", "2024-01-23", "홍보부", true );
+        System.out.println(vo.toString());
+    }
+}
