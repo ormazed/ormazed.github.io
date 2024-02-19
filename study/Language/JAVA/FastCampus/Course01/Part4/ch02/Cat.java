da580f7cdaf84f860bcfadd4687694b6e35ffa57
ormazed <68933277+ormazed@users.noreply.github.com>
2024-02-20 00:04:58 +0900

    Create Cat.java

---
+++ study/Language/JAVA/FastCampus/Course01/Part4/ch02/Cat.java
@@ -0,0 +1,11 @@
+package fc.java.model;
+
+public class Cat {
+    public void eat(){
+        System.out.println("고양이가 먹는다.");
+    }
+    public void night(){
+        System.out.println("눈에서 빛이난다.");
+    }
+}
+
