fce24f3d23c35be9055de77db69059415fd47d77
ormazed <68933277+ormazed@users.noreply.github.com>
2024-02-20 12:29:57 +0900

    Create PolyTest.java

---
+++ study/Language/JAVA/FastCampus/Course01/Part4/ch04/PolyTest.java
@@ -0,0 +1,34 @@
+package fc.java.part4;
+
+import fc.java.model.Animal;
+import fc.java.model.Cat;
+import fc.java.model.Dog;
+
+public class PolyTest {
+    public static void main(String[] args) {
+        // Upcasting 으로 객체를 생성
+        // 상속관계, 재정의(Override), 동적바인딩
+        // 다형성(message polymorphism)
+        // 상위클래스가 도일한 메시지로 하위클래스를 서로 다르게 동작시키는 객체지향 원리
+        Animal ani = new Animal();
+        ani.eat();
+
+        // Upcating
+        ani = new Dog();
+        ani.eat(); // 실행시점에서 사용될(호출될) 메서드가 결정되는 바인딩(동적바인딩)
+
+        // Downcasting
+        Dog d = (Dog) ani;
+        d.bite();
+
+        // Upcating
+        ani = new Cat();
+        ani.eat(); // 실행시점에서 사용될(호출될) 메서드가 결정되는 바이딩(동적바인딩)
+
+        // Downcasting
+      Cat c = (Cat) ani;
+      c.night();
+
+
+    }
+}
