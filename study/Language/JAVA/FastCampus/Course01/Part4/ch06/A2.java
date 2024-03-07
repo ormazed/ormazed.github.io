b1db271c5adc1d44713fb6323fb36c5febd21180
ormazed <68933277+ormazed@users.noreply.github.com>
2024-03-07 19:56:02 +0900

    A2.java 업데이트

---
+++ study/Language/JAVA/FastCampus/Course01/Part4/ch06/A2.java
@@ -0,0 +1,13 @@
+package java.lang.*;
+
+public class A extends Object {
+	public A(){
+	  super();
+	}
+	public void display(){
+	  System.out.println("A");
+	}
+	public void printGp(){
+	  System.out.println("A2");
+	}
+}
