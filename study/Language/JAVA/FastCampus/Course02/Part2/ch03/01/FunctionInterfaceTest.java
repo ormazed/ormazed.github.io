3862c919759fd7f6e5faed2ee498322a79ac7540
ormazed <68933277+ormazed@users.noreply.github.com>
2024-04-03 23:54:09 +0900

    Create FunctionInterfaceTest.java

---
+++ study/Language/JAVA/FastCampus/Course02/Part2/ch03/01/FunctionInterfaceTest.java
@@ -0,0 +1,15 @@
+package fc.java.course2.part2;
+
+import fc.java.model2.MathOperation;
+
+public class FunctionInterfaceTest implements MathOperation {
+    public static void main(String[] args) {
+        MathOperation mo = new FunctionInterfaceTest();
+        int result = mo.operation(20,50);
+        System.out.println("result = " + result);
+    }
+    @Override
+    public int operation(int x, int y) {
+        return x+y;
+    }
+}
