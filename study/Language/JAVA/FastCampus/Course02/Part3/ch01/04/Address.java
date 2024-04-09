4c61ccdf0a44cd28f62f5f01dbdb5394f86f7e6c
ormazed <68933277+ormazed@users.noreply.github.com>
2024-04-09 22:28:26 +0900

    Create Address.java

---
+++ study/Language/JAVA/FastCampus/Course02/Part3/ch01/04/Address.java
@@ -0,0 +1,30 @@
+package fc.java.model3;
+
+public class Address {
+    private String city;
+    private String country;
+
+    public Address() {
+    }
+
+    public Address(String city, String country) {
+        this.city = city;
+        this.country = country;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
