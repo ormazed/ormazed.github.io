981b8ad5111198e442a62fb8fd6a3f5dc83082c6
ormazed <68933277+ormazed@users.noreply.github.com>
2024-04-21 13:44:02 +0900

    Add files via upload

--- study/Language/JAVA/FastCampus/Course03/ch02/restapi/model/restapi/RestApiApplication.java
+++ study/Language/JAVA/FastCampus/Course03/ch02/restapi/model/restapi/RestApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.restapi;
+package com.example.restapi.model.restapi;
 
 
 import org.springframework.boot.SpringApplication;
--- study/Language/JAVA/FastCampus/Course03/ch02/restapi/model/restapi/model/UserRequest.java
+++ study/Language/JAVA/FastCampus/Course03/ch02/restapi/model/restapi/model/UserRequest.java
@@ -1,4 +1,4 @@
-package com.example.restapi.model;
+package com.example.restapi.model.restapi.model;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
