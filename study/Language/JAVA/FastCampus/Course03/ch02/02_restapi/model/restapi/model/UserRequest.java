fe63b17a38619cb1ffb99f5dc2c475bdcee3e0ce
ormazed <68933277+ormazed@users.noreply.github.com>
2024-04-21 13:46:33 +0900

    Add files via upload

---
+++ study/Language/JAVA/FastCampus/Course03/ch02/02_restapi/model/restapi/model/UserRequest.java
@@ -0,0 +1,19 @@
+package com.example.restapi.model.restapi.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class) // 관련 변수들의 클래스 변수들은 snake case 로 매핑하겠다는 의미임.
+public class UserRequest {
+    private String userName;
+    private Integer userAge;
+    private String email;
+    private Boolean isKorean;
+}
