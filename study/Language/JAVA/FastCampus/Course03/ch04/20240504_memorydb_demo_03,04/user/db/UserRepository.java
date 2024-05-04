bd38886732ddb0e4da6f3fc50f83ffa9768802d6
ormazed <68933277+ormazed@users.noreply.github.com>
2024-05-04 14:24:59 +0900

    Add files via upload

---
+++ study/Language/JAVA/FastCampus/Course03/ch04/20240504_memorydb_demo_03,04/user/db/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.user.db;
+
+import com.example.demo.db.SimpleDataRepository;
+import com.example.demo.user.model.UserEntity;
+import org.springframework.stereotype.Service;
+
+import javax.lang.model.util.SimpleElementVisitor6;
+
+@Service
+public class UserRepository extends SimpleDataRepository<UserEntity, Long>{
+
+}
