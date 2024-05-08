2c898d3a4ffd3ff79f626f8f6cff60de1f579b7e
ormazed <68933277+ormazed@users.noreply.github.com>
2024-05-08 18:02:46 +0900

    Add files via upload

---
+++ study/Language/JAVA/FastCampus/Course03/ch08/20240508_simpleboard_01/post/controller/PostApiController.java
@@ -0,0 +1,30 @@
+package com.example.simpleboard.post.controller;
+
+
+import com.example.simpleboard.post.db.PostEntity;
+import com.example.simpleboard.post.model.PostRequest;
+import com.example.simpleboard.post.service.PostService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequestMapping("/api/post")
+@RestController
+@RequiredArgsConstructor
+public class PostApiController {
+
+    private final PostService postService;
+    @PostMapping("")
+    public PostEntity create(
+            @Valid
+            @RequestBody PostRequest postRequest
+    ){
+        return postService.create(postRequest);
+    }
+}
